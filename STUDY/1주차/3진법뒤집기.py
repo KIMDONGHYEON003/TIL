3956b4d23cc0a7f5f8c4c024805b181787a40350
김동현 <budkim1124@naver.com>
2021-08-05 23:24:37 +0900

    0805

---
+++ STUDY/1주차/3진법뒤집기.py
@@ -0,0 +1,17 @@
+def solution(n):
+    answer = 0
+    tmp_list = []
+    n = int(input())
+
+    while True:
+        if n == 0:
+            return tmp_list
+            
+        else:
+            n = n // 3
+            tmp_list.append(n%3)
+        break
+
+        
+print(solution(45))
+    
