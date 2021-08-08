700fb4c21932aeaa132bf97b744f7b6bafdb4b30
김동현 <budkim1124@naver.com>
2021-08-08 17:44:06 +0900

    0808

--- STUDY/1주차/퇴사.py
+++ STUDY/1주차/퇴사.py
@@ -1,2 +1,19 @@
 
 
+n = int(input())
+T, P = [0 for i in range(n+1)], [0 for i in range(n+1)]
+
+for i in range(n):
+    a,b = map(int, input().split())
+    T[i] = a
+    P[i] = b
+
+
+result =[0 for i in range(n+1)]
+
+for i in range(len(T)-2, -1, -1):      
+    if T[i]+i <= n:       
+        result[i] = max(P[i] + result[i + T[i]], result[i+1])   
+    else:                
+        result[i] = result[i+1]
+print(result[0])
